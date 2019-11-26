cdce6b433e02149ed8a4565fdfdd9bcf40dca7eb
LautaroCerruti <lautarocerruti7@gmail.com>
2019-11-26 19:57:05 -0300

    Changed Variables and function names

--- PythonPart/ResolvedorLaberinto.py
+++ PythonPart/ResolvedorLaberinto.py
@@ -1,11 +1,11 @@
 import sys
 
-def LeerLaberinto(fileName, matrix):
+def leerLaberinto(fileName, matrix):
     iteradorLineas = 0
-    Entrada = open(fileName,"r")
-    Lineas = Entrada.readlines()
-    Entrada.close()
-    for linea in Lineas:
+    entrada = open(fileName,"r")
+    lineas = entrada.readlines()
+    entrada.close()
+    for linea in lineas:
         linea = list(linea)
         matrix.append(linea[:-1])
         if linea.count("I") != 0:
@@ -17,14 +17,14 @@ def LeerLaberinto(fileName, matrix):
         iteradorLineas += 1
     return (yInicio, xInicio), (yObjetivo, xObjetivo)
 
-def nodeAnalizer(Node, SumaX, SumaY, fakeQueue, solucion):
-            if len(solucion[Node[0]+SumaX][Node[1]+SumaY]) == 0: 
-                solucion[Node[0]+SumaX][Node[1]+SumaY] = solucion[Node[0]][Node[1]].copy()
-                solucion[Node[0]+SumaX][Node[1]+SumaY].append((Node[0]+SumaX, Node[1]+SumaY))
-                fakeQueue.append((Node[0]+SumaX,Node[1]+SumaY))
-            elif len(solucion[Node[0]+SumaX][Node[1]+SumaY]) > (len(solucion[Node[0]][Node[1]])+1):
-                solucion[Node[0]+SumaX][Node[1]+SumaY] = solucion[Node[0]][Node[1]].copy()
-                solucion[Node[0]+SumaX][Node[1]+SumaY].append((Node[0]+SumaX, Node[1]+SumaY))
+def nodeAnalizer(node, SumaX, SumaY, fakeQueue, solucion):
+            if len(solucion[node[0]+SumaX][node[1]+SumaY]) == 0: 
+                solucion[node[0]+SumaX][node[1]+SumaY] = solucion[node[0]][node[1]].copy()
+                solucion[node[0]+SumaX][node[1]+SumaY].append((node[0]+SumaX, node[1]+SumaY))
+                fakeQueue.append((node[0]+SumaX,node[1]+SumaY))
+            elif len(solucion[node[0]+SumaX][node[1]+SumaY]) > (len(solucion[node[0]][node[1]])+1):
+                solucion[node[0]+SumaX][node[1]+SumaY] = solucion[node[0]][node[1]].copy()
+                solucion[node[0]+SumaX][node[1]+SumaY].append((node[0]+SumaX, node[1]+SumaY))
 
 def BFS(matrix, puntoInicial):
     alto = len(matrix)
@@ -37,38 +37,38 @@ def BFS(matrix, puntoInicial):
     fakeQueue = [puntoInicial]
     solucion[puntoInicial[0]][puntoInicial[1]] = [puntoInicial]
     while len(fakeQueue) > 0:
-        Node = fakeQueue[0]
+        node = fakeQueue[0]
         fakeQueue.pop(0)
-        if Node[0] != 0 and matrix[Node[0]-1][Node[1]] != "1":
-            nodeAnalizer(Node, -1, 0, fakeQueue, solucion)
-        if Node[1] != 0 and matrix[Node[0]][Node[1]-1] != "1":
-            nodeAnalizer(Node, 0, -1, fakeQueue, solucion)
-        if Node[0] != (alto-1) and matrix[Node[0]+1][Node[1]] != "1":
-            nodeAnalizer(Node, 1, 0, fakeQueue, solucion)
-        if Node[1] != (largo-1) and matrix[Node[0]][Node[1]+1] != "1":
-            nodeAnalizer(Node, 0, 1, fakeQueue, solucion)
+        if node[0] != 0 and matrix[node[0]-1][node[1]] != "1":
+            nodeAnalizer(node, -1, 0, fakeQueue, solucion)
+        if node[1] != 0 and matrix[node[0]][node[1]-1] != "1":
+            nodeAnalizer(node, 0, -1, fakeQueue, solucion)
+        if node[0] != (alto-1) and matrix[node[0]+1][node[1]] != "1":
+            nodeAnalizer(node, 1, 0, fakeQueue, solucion)
+        if node[1] != (largo-1) and matrix[node[0]][node[1]+1] != "1":
+            nodeAnalizer(node, 0, 1, fakeQueue, solucion)
     return solucion
 
-def BuscaCamino(matrix, inicio, objetivo):
+def buscaCamino(matrix, inicio, objetivo):
     Caminos = BFS(matrix, inicio)
     return(Caminos[objetivo[0]][objetivo[1]])
 
-def ImprimeResultado(resultado, archivoSalida):
+def imprimeResultado(resultado, archivoSalida):
     solucion = []
     for nodo in resultado:
         solucion.append((nodo[1]+1,nodo[0]+1))
     print(solucion)
 
 def Main():
-    ArchivoEntrada = sys.argv[1]
-    ArchivoSalida = sys.argv[2]
+    archivoEntrada = sys.argv[1]
+    archivoSalida = sys.argv[2]
     matrix = []
-    inicio, objetivo = LeerLaberinto(ArchivoEntrada, matrix)
-    Resultado = BuscaCamino(matrix, inicio, objetivo)
-    if len(Resultado) == 0:
+    inicio, objetivo = leerLaberinto(archivoEntrada, matrix)
+    resultado = buscaCamino(matrix, inicio, objetivo)
+    if len(resultado) == 0:
         print("No tiene solucion")
     else:
-        ImprimeResultado(Resultado, ArchivoSalida)
+        imprimeResultado(resultado, archivoSalida)
 
 if __name__ == "__main__":
     Main()
